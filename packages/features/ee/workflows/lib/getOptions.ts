bbc74d816f32c419c652aafb5e89659ca07dc8fc
Patranto Bagus Prabowo <prabowo@xprenia.com>
2023-04-18 14:01:02 +0700

    remove email limiter

--- packages/features/ee/workflows/lib/getOptions.ts
+++ packages/features/ee/workflows/lib/getOptions.ts
@@ -1,20 +1,18 @@
-import { WorkflowActions } from "@prisma/client";
-import { TFunction } from "next-i18next";
+import type { TFunction } from "next-i18next";
 
 import { TIME_UNIT, WORKFLOW_ACTIONS, WORKFLOW_TEMPLATES, WORKFLOW_TRIGGER_EVENTS } from "./constants";
 import { isSMSAction } from "./isSMSAction";
 
 export function getWorkflowActionOptions(t: TFunction, isTeamsPlan?: boolean) {
-  return WORKFLOW_ACTIONS.filter((action) => action !== WorkflowActions.EMAIL_ADDRESS) //removing EMAIL_ADDRESS for now due to abuse episode
-    .map((action) => {
-      const actionString = t(`${action.toLowerCase()}_action`);
+  return WORKFLOW_ACTIONS.map((action) => {
+    const actionString = t(`${action.toLowerCase()}_action`);
 
-      return {
-        label: actionString.charAt(0).toUpperCase() + actionString.slice(1),
-        value: action,
-        needsUpgrade: isSMSAction(action) && !isTeamsPlan,
-      };
-    });
+    return {
+      label: actionString.charAt(0).toUpperCase() + actionString.slice(1),
+      value: action,
+      needsUpgrade: isSMSAction(action) && !isTeamsPlan,
+    };
+  });
 }
 
 export function getWorkflowTriggerOptions(t: TFunction) {
